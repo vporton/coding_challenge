02776617b88469a36e23257200b760e3849c78c0
anjali <anjali.deolapure@gmail.com>
2019-04-18 22:09:07 -0500

    skeleton flask app

---
+++ app/routes.py
@@ -0,0 +1,17 @@
+import logging
+
+import flask
+from flask import Response
+
+app = flask.Flask("user_profiles_api")
+logger = flask.logging.create_logger(app)
+logger.setLevel(logging.INFO)
+
+
+@app.route("/health-check", methods=["GET"])
+def health_check():
+    """
+    Endpoint to health check API
+    """
+    app.logger.info("Health Check!")
+    return Response("All Good!", status=200)
