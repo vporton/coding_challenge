41141c6e890238b036a68bc88267c21a02682f2b
Victor Porton <porton@narod.ru>
2019-10-12 11:22:38 +0300

    request handling

--- aggregate/urls.py
+++ aggregate/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+import from_git.views
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # path('admin/', admin.site.urls),
+    path('health-check', from_git.views.GitAggregator.as_view())
 ]
--- from_git/views.py
+++ from_git/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import views
+from rest_framework.response import Response
 
-# Create your views here.
+
+class GitAggregator(views.APIView):
+    """The class that shows aggregation of several Git hostings data."""
+    def get(self, request):
+        return Response({})
