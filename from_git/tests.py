f90a50f0bc0c43f2e5855c37b60e35bf6d7dc120
Victor Porton <porton@narod.ru>
2019-10-16 15:47:16 +0300

    first unit test

--- from_git/tests.py
+++ from_git/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your templates here.
+from from_git.aggregator import aggregate_one
+
+
+class DownloadingTestCase(TestCase):
+    def test_aggregate_one(self):
+        for url in ["https://github.com/mailchimp", "https://bitbucket.org/mailchimp"]:
+            data = aggregate_one(url)
+            assert(data['originalRepos'] > 1)
+            #assert(missing.empty())
+        for url in ["https://github.com/jsdkgjhds", "https://bitbucket.org/hsjdhtjehrje"]:
+            data = aggregate_one(url)
+            assert(data is None)
+            #assert(len(missing) == 1)
