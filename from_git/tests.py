f5fc577b240c372b07963be718cbd7a237c07680
Victor Porton <porton@narod.ru>
2019-10-16 15:56:53 +0300

    aggregation tests

--- from_git/tests.py
+++ from_git/tests.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 
-from from_git.aggregator import aggregate_one
+from from_git.aggregator import aggregate_one, WrongURLException, aggregate_data
 
 
 class DownloadingTestCase(TestCase):
@@ -8,8 +8,20 @@ class DownloadingTestCase(TestCase):
         for url in ["https://github.com/mailchimp", "https://bitbucket.org/mailchimp"]:
             data = aggregate_one(url)
             assert(data['originalRepos'] > 1)
-            #assert(missing.empty())
+            assert("python" in data['langs'])
         for url in ["https://github.com/jsdkgjhds", "https://bitbucket.org/hsjdhtjehrje"]:
             data = aggregate_one(url)
             assert(data is None)
             #assert(len(missing) == 1)
+        self.assertRaises(WrongURLException, aggregate_one, "hjhjk")
+
+    def test_aggregate_data(self):
+        data, missing = aggregate_data(["https://github.com/mailchimp",
+                                        "https://bitbucket.org/mailchimp",
+                                        "https://github.com/hejwhrejkhw",
+                                        "https://bitbucket.org/hsjdhtjehrje"])
+        assert(data['originalRepos'] > 1)
+        assert(data['forkedRepos'] > 1)
+        assert("python" in data['langs'])
+        assert("ecommerce" in data['topics'])
+        assert(missing == ["https://github.com/hejwhrejkhw", "https://bitbucket.org/hsjdhtjehrje"])
