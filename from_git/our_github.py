b66d58fb0f9671b623fe164519d64d7b63fa1e36
Victor Porton <porton@narod.ru>
2019-10-16 15:28:36 +0300

    GitHub pagination debugged

--- from_git/our_github.py
+++ from_git/our_github.py
@@ -83,7 +83,6 @@ def get_repositories_for_org(client, org):
             }
         }
         ''' % (json.dumps(org), number_of_repos_in_query, after_str))
-        # TODO: Debug pagination
         data = json.loads(j)
         if 'errors' in data:
             if data['data']['organization'] is None:
